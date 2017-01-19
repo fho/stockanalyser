0ab09dd5c2979c3306b83dfb08238fef40292ea9
Fabian Holler <mail@fholler.de>
2017-01-19 19:48:02 +0100

    virtualenv: install lxml in virtualenv
    the lxml package is required for the onvista scraper module

--- create-virtualenv.sh
+++ create-virtualenv.sh
@@ -2,6 +2,6 @@
 
 virtualenv -p /usr/bin/python3.6 venv
 source venv/bin/activate 
-pip install money pytest pep8 pylint
+pip install money pytest pep8 pylint lxml
 
 echo "Switch to Virtualenv with: 'source venv/bin/activate'"
